9ff17b4af7ffa4fb153744385411c595075595d1
joshburns <92578127+jbazel@users.noreply.github.com>
2023-02-03 18:25:49 +0000

    working noramalization

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
+import base64
+
 import pyaudio as pa
 import numpy as np
 from collections import deque
 import threading
 import time
+from sklearn import preprocessing as p
+
+import OpenGL
+from OpenGL.raw.GL import *
+from OpenGL.raw.GL.VERSION.GL_1_0 import *
+from OpenGL.raw.GLU import *
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+global fps
+fps = 24
+
 def main():
-    q = deque()
+    fig = plt.figure()
+    frames = deque()
     def audio_buffer_generator():
         p = pa.PyAudio()
         stream = p.open(format=pa.paInt16,
                         channels=1,
                         rate=44100,
                         input=True,
-                        frames_per_buffer=1024)
+                        frames_per_buffer=fps)
+
         while True:
-            data = stream.read(1024)
+            data = stream.read(1024, exception_on_overflow=False)
             yield data
             stream.stop_stream()
             stream.close()
@@ -21,20 +38,26 @@ def main():
 
 
     def audioStream():
-
         while True:
-            time.sleep(1/60)
-            q.append(audio_buffer_generator())
+            time.sleep(1/fps)
+            data = audio_buffer_generator()
+            frameArray = np.frombuffer(next(data), dtype=np.int16)
+            frames.append(frameArray)
 
     def processBuffer():
         while True:
-            if q:
-                data = q.popleft()
-                print(data.get_audio_features())
+            if frames:
+                frame = np.absolute(frames.popleft())
+                frame_norm = p.MinMaxScaler().fit_transform(frame.reshape(-1, 1))
+                frame[np.isnan(frame)] = 0
+                print(frame_norm)
+                print("Frame length: ", len(frame))
+
 
 
     threading.Thread(target=audioStream).start()
     threading.Thread(target=processBuffer).start()
+
 
 
 
