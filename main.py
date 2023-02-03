6d7d14621d4251c5b6ecf3cab037f234ecd6eb1b
joshburns <92578127+jbazel@users.noreply.github.com>
2023-02-03 15:33:09 +0000

    IT WORKS WOOOO

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import pyaudio
+import pyaudio as pa
 import numpy as np
+
 
 def main():
     def audio_buffer_generator():
-        p = pyaudio.PyAudio()
-        stream = p.open(format=pyaudio.paInt16,
+        p = pa.PyAudio()
+        stream = p.open(format=pa.paInt16,
                         channels=1,
                         rate=44100,
                         input=True,
                         frames_per_buffer=1024)
-            
+
         while True:
             data = stream.read(1024)
             yield data
@@ -19,7 +20,7 @@ def main():
 
     audio_buffer = audio_buffer_generator()
     print(audio_buffer)
-    
+
 
 if __name__ == "__main__":
     main()
