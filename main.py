40877fcf78de824ef016d3635755ebcd9fa44283
joshburns <92578127+jbazel@users.noreply.github.com>
2023-02-03 18:55:13 +0000

    kinda working visualisation

--- main.py
+++ main.py
@@ -14,13 +14,18 @@ from OpenGL.raw.GLU import *
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from tkinter import *
+
+
+global run
+run = True
 
 global fps
-fps = 24
+fps = 60
 
 def main():
-    fig = plt.figure()
     frames = deque()
+    ready_frames = deque()
     def audio_buffer_generator():
         p = pa.PyAudio()
         stream = p.open(format=pa.paInt16,
@@ -29,33 +34,36 @@ def main():
                         input=True,
                         frames_per_buffer=fps)
 
-        while True:
+
+        while run:
             data = stream.read(1024, exception_on_overflow=False)
-            yield data
-            stream.stop_stream()
-            stream.close()
-            p.terminate()
-
-
-    def audioStream():
-        while True:
-            time.sleep(1/fps)
-            data = audio_buffer_generator()
-            frameArray = np.frombuffer(next(data), dtype=np.int16)
+            time.sleep(1 / fps)
+            frameArray = np.frombuffer(data, dtype=np.int16)
             frames.append(frameArray)
 
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+
+    # def audioStream():
+    #     while run:
+    #         time.sleep(1/fps)
+    #         data = audio_buffer_generator()
+    #         frameArray = np.frombuffer(next(data), dtype=np.int16)
+    #         frames.append(frameArray)
+
     def processBuffer():
-        while True:
+        while run:
             if frames:
                 frame = np.absolute(frames.popleft())
-                frame_norm = p.MinMaxScaler().fit_transform(frame.reshape(-1, 1))
-                frame[np.isnan(frame)] = 0
-                print(frame_norm)
-                print("Frame length: ", len(frame))
+                frame_norm = p.MinMaxScaler().fit_transform(frame.reshape(-1, 1)).flatten().tolist()
+                plt.plot(frame_norm)
+                plt.show()
 
 
 
-    threading.Thread(target=audioStream).start()
+    threading.Thread(target=audio_buffer_generator).start()
     threading.Thread(target=processBuffer).start()
 
 
